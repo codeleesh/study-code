93ccbdd6c7037c3e9c11a462336cf3d3abb3321b
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-08-04 23:15:08 +0900

    Add ServiceImpl

---
+++ springboot2-community/src/main/java/com/lovethefeel/springboot2/havi/service/BookServiceImpl.java
@@ -0,0 +1,14 @@
+package com.lovethefeel.springboot2.havi.service;
+
+import com.lovethefeel.springboot2.havi.domain.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService{
+    @Override
+    public List<Book> getBookList() {
+        return null;
+    }
+}
