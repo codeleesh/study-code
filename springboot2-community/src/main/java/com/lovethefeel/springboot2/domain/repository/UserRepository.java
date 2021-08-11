fd4b8d4e67bd453d1b84351aba6697d9272a2cb6
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-08-11 23:50:44 +0900

    Add User

--- springboot2-community/src/main/java/com/lovethefeel/springboot2/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lovethefeel.springboot2.domain.repository;
-
-import com.lovethefeel.springboot2.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Long, User> {
-}
