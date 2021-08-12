ec88fb5bf534e4497f0102ed4df90acee1d97095
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-08-12 21:23:17 +0900

    Add CommandLineRunner

--- springboot2-community/src/main/java/com/lovethefeel/springboot2/Springboot2CommunityApplication.java
+++ springboot2-community/src/main/java/com/lovethefeel/springboot2/Springboot2CommunityApplication.java
@@ -1,7 +1,17 @@
 package com.lovethefeel.springboot2;
 
+import com.lovethefeel.springboot2.domain.Board;
+import com.lovethefeel.springboot2.domain.User;
+import com.lovethefeel.springboot2.domain.enums.BoardType;
+import com.lovethefeel.springboot2.repository.BoardRepository;
+import com.lovethefeel.springboot2.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Springboot2CommunityApplication {
@@ -10,4 +20,28 @@ public class Springboot2CommunityApplication {
 		SpringApplication.run(Springboot2CommunityApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) {
+		return (args) -> {
+			User user = userRepository.save(User.builder()
+					.name("havi")
+					.password("test")
+					.email("havi@gmail.com")
+					.createdDate(LocalDateTime.now())
+					.build()
+			);
+
+			IntStream.rangeClosed(1, 200).forEach(index ->
+					boardRepository.save(Board.builder()
+							.title("게시글"+index)
+							.subTitle("순서"+index)
+							.content("콘텐츠")
+							.boardType(BoardType.free)
+							.createdDate(LocalDateTime.now())
+							.updatedDate(LocalDateTime.now())
+							.user(user)
+							.build())
+			);
+		};
+	}
 }
