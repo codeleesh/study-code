480167e593a5bd059ba0669329383855f9b49106
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-08-15 01:09:54 +0900

    Change BoardType test

--- springboot2-community/src/test/java/com/lovethefeel/springboot2/domain/enums/BoardTypeTest.java
+++ springboot2-community/src/test/java/com/lovethefeel/springboot2/domain/enums/BoardTypeTest.java
@@ -1,6 +1,5 @@
-package com.lovethefeel.springboot2.domain;
+package com.lovethefeel.springboot2.domain.enums;
 
-import com.lovethefeel.springboot2.domain.enums.BoardType;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
