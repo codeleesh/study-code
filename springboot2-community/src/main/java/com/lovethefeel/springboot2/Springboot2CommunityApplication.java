7121208444ab8390bbb4f1102827d31c234bf0cd
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-08-17 23:45:09 +0900

    Add spring-security-oauth2

--- springboot2-community/src/main/java/com/lovethefeel/springboot2/Springboot2CommunityApplication.java
+++ springboot2-community/src/main/java/com/lovethefeel/springboot2/Springboot2CommunityApplication.java
@@ -5,19 +5,32 @@ import com.lovethefeel.springboot2.domain.User;
 import com.lovethefeel.springboot2.domain.enums.BoardType;
 import com.lovethefeel.springboot2.repository.BoardRepository;
 import com.lovethefeel.springboot2.repository.UserRepository;
+import com.lovethefeel.springboot2.resolver.UserArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class Springboot2CommunityApplication {
+public class Springboot2CommunityApplication implements WebMvcConfigurer {
+
+	@Autowired
+	private UserArgumentResolver userArgumentResolver;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2CommunityApplication.class, args);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> handlerMethodArgumentResolverList) {
+		handlerMethodArgumentResolverList.add(userArgumentResolver);
 	}
 
 	@Bean
