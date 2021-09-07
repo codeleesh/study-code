7aa08b6c7d771ea343d7c6d0d963a5663716574c
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-09-07 22:45:30 +0900

    Change test annotation WebMvcTest

--- springboot2-data-jpa/src/test/java/com/lovethefeel/springboot/controller/UserControllerTest.java
+++ springboot2-data-jpa/src/test/java/com/lovethefeel/springboot/controller/UserControllerTest.java
@@ -1,60 +1,75 @@
 package com.lovethefeel.springboot.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.lovethefeel.springboot.domain.user.UserSequence;
 import com.lovethefeel.springboot.dto.user.UserSequenceRequest;
-import com.lovethefeel.springboot.repository.user.SpringDataJpaUserSequenceRepository;
-import org.assertj.core.api.Assertions;
+import com.lovethefeel.springboot.service.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.transaction.Transactional;
-import java.util.Optional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private SpringDataJpaUserSequenceRepository springDataJpaUserSequenceRepository;
-
-    @Autowired
-    private WebApplicationContext context;
 
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    UserService userService;
+
     @Test
-    @Transactional
     public void testSave() throws Exception {
+
+//        // given
+//        UserSequenceRequest userSequenceRequest = UserSequenceRequest.builder()
+//                .name("테스트")
+//                .build();
+//
+//        String url = "http://localhost:"+ port + "/api/v1/user";
+//
+//        // when
+//        mvc.perform(post(url)
+//                .contentType(MediaType.APPLICATION_JSON)
+//                .content(new ObjectMapper().writeValueAsString(userSequenceRequest)))
+//                .andExpect(status().isOk());
 
         // given
         UserSequenceRequest userSequenceRequest = UserSequenceRequest.builder()
                 .name("테스트")
                 .build();
 
-        String url = "http://localhost:"+ port + "/api/v1/user";
+        // when-then
+        mvc.perform(post("/api/v1/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(userSequenceRequest)))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+//                .andExpect(MockMvcResultMatchers.jsonPath("$.userId").value(1))
 
-        // when
-        mvc.perform(post(url)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(new ObjectMapper().writeValueAsString(userSequenceRequest)))
-                .andExpect(status().isOk());
+        //when
+        final ResultActions actions = mvc.perform(post("/api/v1/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(userSequenceRequest)))
+                .andDo(print());
 
-        // then
-        Optional<UserSequence> userSequence = springDataJpaUserSequenceRepository.findByName("테스트");
-        Assertions.assertThat(userSequence).isNotEmpty();
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("id").value(1L))
+                .andExpect(jsonPath("title").value("title"))
+                .andExpect(jsonPath("price").value(1000D))
+        ;
+
     }
 }
