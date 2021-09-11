28bd597fc13e2959ee684c52e8df8af6dfa31463
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-09-11 22:39:31 +0900

    Remove comment

--- springboot2-data-jpa/src/main/java/com/lovethefeel/springboot/config/DatabaseConfig.java
+++ springboot2-data-jpa/src/main/java/com/lovethefeel/springboot/config/DatabaseConfig.java
@@ -6,22 +6,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseConfig {
 
-    private final SpringDataJpaUserIdentityRepository springDataJpaUserIdentityRepository;
+    final SpringDataJpaUserIdentityRepository springDataJpaUserIdentityRepository;
 
     public DatabaseConfig(SpringDataJpaUserIdentityRepository springDataJpaUserIdentityRepository) {
         this.springDataJpaUserIdentityRepository = springDataJpaUserIdentityRepository;
     }
-
-
-//    private EntityManager em;
-//
-//    @Autowired
-//    public DatabaseConfig(EntityManager em) {
-//        this.em = em;
-//    }
-//
-//    @Bean
-//    public UserRepository userRepository() {
-//        return new SpringDataJpaUserRepository(em);
-//    }
 }
