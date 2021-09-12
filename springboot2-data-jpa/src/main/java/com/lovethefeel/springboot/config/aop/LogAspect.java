eb54b6c7a5c81a7f2e27321ad818ea58bff57788
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-09-12 22:13:18 +0900

    Remove warning

--- springboot2-data-jpa/src/main/java/com/lovethefeel/springboot/config/aop/LogAspect.java
+++ springboot2-data-jpa/src/main/java/com/lovethefeel/springboot/config/aop/LogAspect.java
@@ -3,7 +3,6 @@ package com.lovethefeel.springboot.config.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.reflect.SourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -24,7 +23,6 @@ public class LogAspect {
         String className = proceedingJoinPoint.getSignature().getName();
 
         stopWatch.stop();
-        //logger.info(stopWatch.prettyPrint());
         logger.info("{} elapsed time :: {}", className, stopWatch.getTotalTimeMillis());
 
         return proceed;
