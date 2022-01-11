2df762718fe1b2dbf8c8734bd09fae0a4dcc8dcd
lovethefeel <lovethefeel@gmail.com>
2022-01-12 02:34:27 +0900

    feat: 스프링 애플리케이션 메인 실행 클래스

---
+++ spring-boot-cache/src/main/java/com/lovethefeel/springboot/SpringBootCacheApplication.java
@@ -0,0 +1,13 @@
+package com.lovethefeel.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCacheApplication.class, args);
+    }
+
+}
