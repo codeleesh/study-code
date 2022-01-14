e1e84cde8b5cce6c46d89a0fd88fb46c1c023205
lovethefeel <lovethefeel@gmail.com>
2022-01-15 03:19:18 +0900

    test: 가격 필드 추가

--- spring-boot-http/src/test/java/com/lovethefeel/http/application/BookServiceTest.java
+++ spring-boot-http/src/test/java/com/lovethefeel/http/application/BookServiceTest.java
@@ -3,7 +3,7 @@ package com.lovethefeel.http.application;
 import com.lovethefeel.http.domain.Book;
 import com.lovethefeel.http.domain.BookRepository;
 import com.lovethefeel.http.dto.BookChangeRequest;
-import com.lovethefeel.http.dto.BookNameChangeReuqest;
+import com.lovethefeel.http.dto.BookPriceChangeReuqest;
 import com.lovethefeel.http.dto.BookRequest;
 import com.lovethefeel.http.dto.BookResponse;
 import org.assertj.core.util.Lists;
@@ -34,8 +34,8 @@ class BookServiceTest {
     @DisplayName("책을 등록한다.")
     @Test
     void registerTest() {
-        final BookRequest bookRequest = BookRequest.from("자바의정석");
-        final Book book = Book.of(1L, "자바의정석");
+        final BookRequest bookRequest = BookRequest.of("자바의정석", 15000);
+        final Book book = Book.of(1L, "자바의정석", 15000);
         given(bookRepository.save(any())).willReturn(book);
 
         final BookResponse bookResponse = bookService.registerBook(bookRequest);
@@ -46,7 +46,7 @@ class BookServiceTest {
     @DisplayName("책을 단건 조회한다.")
     @Test
     void findBookTest() {
-        final Book book = Book.of(1L, "자바의정석");
+        final Book book = Book.of(1L, "자바의정석", 15000);
         given(bookRepository.findById(anyLong())).willReturn(Optional.of(book));
 
         final BookResponse bookResponse = bookService.findBook(1L);
@@ -54,11 +54,22 @@ class BookServiceTest {
         assertThat(bookResponse.getName()).isEqualTo(book.getName());
     }
 
+    @DisplayName("책의 ID와 이름을 이용하여 조회한다.")
+    @Test
+    void findBookIdAndNameTest() {
+        final Book book = Book.of(1L, "자바의정석", 15000);
+        given(bookRepository.findByIdAndName(anyLong(), any())).willReturn(book);
+
+        final BookResponse bookResponse = bookService.findBookByIdAndName(1L, "자바의정석");
+
+        assertThat(bookResponse.getName()).isEqualTo(book.getName());
+    }
+
     @DisplayName("책을 다건 조회한다.")
     @Test
     void findBookAllTest() {
-        final Book book1 = Book.of(1L, "자바의정석");
-        final Book book2 = Book.of(2L, "클린코드");
+        final Book book1 = Book.of(1L, "자바의정석", 15000);
+        final Book book2 = Book.of(2L, "클린코드", 20000);
         final List<Book> books = Lists.newArrayList(book1, book2);
         given(bookRepository.findAll()).willReturn(books);
 
@@ -70,25 +81,26 @@ class BookServiceTest {
     @DisplayName("책 정보를 수정한다.")
     @Test
     void changeBookTest() {
-        final BookChangeRequest bookChangeRequest = BookChangeRequest.from("클린코드");
-        final Book book = Book.of(1L, "자바의정석");
+        final BookChangeRequest bookChangeRequest = BookChangeRequest.of("클린코드", 20000);
+        final Book book = Book.of(1L, "자바의정석", 15000);
+        given(bookRepository.existsById(anyLong())).willReturn(true);
         given(bookRepository.findById(anyLong())).willReturn(Optional.of(book));
 
-        bookService.changeBook(1L, bookChangeRequest);
+        final BookResponse bookResponse = bookService.changeBook(1L, bookChangeRequest);
 
-        assertThat(book.getName()).isEqualTo(bookChangeRequest.getName());
+        assertThat(bookResponse.getName()).isEqualTo(bookChangeRequest.getName());
     }
 
-    @DisplayName("책 이름을 수정한다.")
+    @DisplayName("책 가격을 수정한다.")
     @Test
-    void changeNameBookTest() {
-        final BookNameChangeReuqest bookNameChangeReuqest = BookNameChangeReuqest.from("클린코드");
-        final Book book = Book.of(1L, "자바의정석");
+    void changePriceBookTest() {
+        final BookPriceChangeReuqest bookPriceChangeReuqest = BookPriceChangeReuqest.from(20000);
+        final Book book = Book.of(1L, "자바의정석", 15000);
         given(bookRepository.findById(anyLong())).willReturn(Optional.of(book));
 
-        final BookResponse bookResponse = bookService.changeNameBook(1L, bookNameChangeReuqest);
+        final BookResponse bookResponse = bookService.changePriceBook(1L, bookPriceChangeReuqest);
 
-        assertThat(bookResponse.getName()).isEqualTo(bookNameChangeReuqest.getName());
+        assertThat(bookResponse.getPrice()).isEqualTo(bookPriceChangeReuqest.getPrice());
     }
 
     @DisplayName("책을 삭제한다.")
