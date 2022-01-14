f376e54c20feba00b73ae91092beb27b30991830
lovethefeel <lovethefeel@gmail.com>
2022-01-15 03:18:20 +0900

    chore: 가격 필드 추가

--- spring-boot-http/src/main/resources/import.sql
+++ spring-boot-http/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO book(id, name) values(1, '자바의정석');
-INSERT INTO book(id, name) values(2, '자바 ORM 표준 JPA 표준 프로그래밍');
-INSERT INTO book(id, name) values(3, '켄트 벡의 구현 팬턴');
-INSERT INTO book(id, name) values(4, '클린 코드');
-INSERT INTO book(id, name) values(5, '리팩토링 2탄');
+INSERT INTO book(id, name, price) values(1, '자바의정석', 15000);
+INSERT INTO book(id, name, price) values(2, '자바 ORM 표준 JPA 표준 프로그래밍', 33000);
+INSERT INTO book(id, name, price) values(3, '켄트 벡의 구현 팬턴', 18000);
+INSERT INTO book(id, name, price) values(4, '클린 코드', 22000);
+INSERT INTO book(id, name, price) values(5, '리팩토링 2탄', 25000);
