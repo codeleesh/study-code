564a8cd0c8508fe25811019e97e9214a67ff8b64
lovethefeel <lovethefeel@gmail.com>
2022-01-15 03:24:55 +0900

    refactor: 가격 필드 추가, equals and hashCode 추가

--- spring-boot-http/src/main/java/com/lovethefeel/http/domain/Book.java
+++ spring-boot-http/src/main/java/com/lovethefeel/http/domain/Book.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -13,31 +14,62 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column
     private String name;
+
+    @Column
+    private long price;
 
     protected Book() {
     }
 
-    private Book(final Long id, final String name) {
+    private Book(final Long id, final String name, final long price) {
         this.id = id;
         this.name = name;
+        this.price = price;
     }
 
-    public static Book of(final Long id, final String name) {
-        return new Book(id, name);
+    public static Book of(final Long id, final String name, final long price) {
+        return new Book(id, name, price);
+    }
+
+    public static Book of(final String name, final long price) {
+        return new Book(null, name, price);
     }
 
     public static Book from(final String name) {
-        return new Book(null, name);
+        return new Book(null, name, 0);
+    }
+
+    public static Book from(final long price) {
+        return new Book(null, null, price);
     }
 
     public void changeName(final Book book) {
         this.name = book.name;
     }
 
+    public void changePrice(final Book book) {
+        this.price = book.price;
+    }
+
     public void changeBook(final Book book) {
         this.name = book.name;
+        this.price = book.price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
@@ -45,6 +77,7 @@ public class Book {
         return "Book{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", price=" + price +
                 '}';
     }
 }
