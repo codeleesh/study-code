332388ee421861383f4e1c90946619ab79e44dd6
lovethefeel <lovethefeel@gmail.com>
2022-01-15 03:21:41 +0900

    refactor: 조회 쿼리 추가

--- spring-boot-http/src/main/java/com/lovethefeel/http/domain/BookRepository.java
+++ spring-boot-http/src/main/java/com/lovethefeel/http/domain/BookRepository.java
@@ -3,4 +3,7 @@ package com.lovethefeel.http.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByIdAndName(final Long id, final String name);
+
+    Book findByName(final String name);
 }
