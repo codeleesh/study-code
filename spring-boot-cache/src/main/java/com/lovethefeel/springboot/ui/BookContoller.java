238d3615ff1365766f58f6422f23e4c545ac9e9f
lovethefeel <lovethefeel@gmail.com>
2022-01-30 23:46:26 +0900

    refactor 생성자 주입 변경, cache 기능 추가

--- spring-boot-cache/src/main/java/com/lovethefeel/springboot/ui/BookContoller.java
+++ spring-boot-cache/src/main/java/com/lovethefeel/springboot/ui/BookContoller.java
@@ -1,30 +1,49 @@
 package com.lovethefeel.springboot.ui;
 
 import com.lovethefeel.springboot.application.BookService;
-import com.lovethefeel.springboot.domain.Book;
-import lombok.AllArgsConstructor;
+import com.lovethefeel.springboot.dto.BookRequest;
+import com.lovethefeel.springboot.dto.BookResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BookContoller {
 
     private final BookService bookService;
 
-    @Cacheable("book")
-    @GetMapping("book")
-    public Mono<Book> findBook() {
-        return bookService.findBook();
+    @Cacheable(cacheNames = "books", key = "#bookId")
+    @GetMapping("book/{bookId}")
+    public ResponseEntity<BookResponse> findBook(@PathVariable("bookId") final Long bookId) {
+        return ResponseEntity.ok(bookService.findBook(bookId));
     }
 
-    @Cacheable("books")
+    @Cacheable(cacheNames = "books")
     @GetMapping("books")
-    public Mono<List<Book>> findBooks() {
-        return bookService.findBookAll();
+    public ResponseEntity<List<BookResponse>> findBooks() {
+        return ResponseEntity.ok(bookService.findBookAll());
+    }
+
+    @CacheEvict(cacheNames = "books", allEntries = true)
+    @PostMapping("book")
+    public ResponseEntity<BookResponse> createBook(final @RequestBody BookRequest bookRequest) {
+        final BookResponse created = bookService.createBook(bookRequest);
+        final URI uri = URI.create("/book/" + created.getId());
+        return ResponseEntity.created(uri)
+                .body(created);
+    }
+
+    @CachePut(cacheNames = "books", key = "#bookId")
+    @PatchMapping("book/{bookId}")
+    public ResponseEntity<BookResponse> updateBook(@PathVariable("bookId") final Long bookId, @RequestBody final BookRequest bookRequest) {
+        final BookResponse response = bookService.updateBook(bookId, bookRequest);
+        return ResponseEntity.ok(response);
     }
 }
--- spring-boot-cache/src/main/java/com/lovethefeel/springboot/ui/BookMemoryController.java
+++ spring-boot-cache/src/main/java/com/lovethefeel/springboot/ui/BookMemoryController.java
@@ -2,23 +2,23 @@ package com.lovethefeel.springboot.ui;
 
 import com.lovethefeel.springboot.application.BookMemoryService;
 import com.lovethefeel.springboot.domain.Book;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BookMemoryController {
 
     private final BookMemoryService bookMemoryService;
 
     @Cacheable("memory_books")
     @GetMapping("memory/books")
-    public Mono<List<Book>> findBooks() {
-        return Mono.just(bookMemoryService.getBooks());
+    public ResponseEntity<List<Book>> findBooks() {
+        return ResponseEntity.ok(bookMemoryService.getBooks());
     }
 }
