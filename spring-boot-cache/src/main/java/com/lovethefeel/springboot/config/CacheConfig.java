85150508b6b62ca050c8b02817b59892d4c9c8cb
lovethefeel <lovethefeel@gmail.com>
2022-01-30 23:43:06 +0900

    refactor useless cacheManager

--- spring-boot-cache/src/main/java/com/lovethefeel/springboot/config/CacheConfig.java
+++ spring-boot-cache/src/main/java/com/lovethefeel/springboot/config/CacheConfig.java
@@ -1,21 +1,9 @@
 package com.lovethefeel.springboot.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
-@Slf4j
 public class CacheConfig {
-
-    @Bean
-    public CacheManager cacheManger() {
-        final SimpleCacheManager cacheManager = new SimpleCacheManager();
-        log.info("CacheManager cacheManger [{}]", cacheManager.getCacheNames());
-        return cacheManager;
-    }
 }
