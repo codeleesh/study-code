90c759a03443d0919c1d3d24963fab6f4ddcd156
lovethefeel <lovethefeel@gmail.com>
2022-02-11 00:13:04 +0900

    refactor 프로젝트 이름 변경으로 삭제

--- commerce/src/main/java/com/lovethefeel/commerce/domain/joinded/JobsRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lovethefeel.commerce.domain.joinded;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobsRepository extends JpaRepository<Jobs, Long> {
-}
--- commerce/src/main/java/com/lovethefeel/commerce/domain/singletable/ItemRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lovethefeel.commerce.domain.singletable;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Long> {
-}
