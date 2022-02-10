29fb4e079c7fa87e388946180237bf4a8c1fbdfe
lovethefeel <lovethefeel@gmail.com>
2022-02-11 00:23:45 +0900

    refactor 사용하지 않는 프로젝트 제거

--- springboot2-community/src/main/java/com/lovethefeel/springboot2/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lovethefeel.springboot2.repository;
-
-import com.lovethefeel.springboot2.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
-}
