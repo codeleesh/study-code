dfb6edfd4c7a0a816c7b6c40c0bbbd82e0401598
lovethefeel <lovethefeel@gmail.com>
2022-04-16 01:05:50 +0900

    init project

---
+++ stream/src/main/java/com/lovethefeel/StreamApplication.java
@@ -0,0 +1,13 @@
+package com.lovethefeel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamApplication.class, args);
+    }
+
+}
---
+++ stream/src/main/java/com/lovethefeel/before/domain/Status.java
@@ -0,0 +1,5 @@
+package com.lovethefeel.before.domain;
+
+public enum Status {
+    ENABLE, DISABLE
+}
