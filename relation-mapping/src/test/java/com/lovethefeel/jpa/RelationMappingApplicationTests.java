df37c66f724f4ebfc5f9fb6b646800f67ffdb77c
lovethefeel <lovethefeel@gmail.com>
2022-04-16 01:40:17 +0900

    feat 연관관계 중간 진행 내용 추가

---
+++ relation-mapping/src/test/java/com/lovethefeel/jpa/RelationMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovethefeel.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationMappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
