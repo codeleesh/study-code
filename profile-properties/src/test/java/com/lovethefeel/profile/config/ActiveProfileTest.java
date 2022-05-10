1ae0cc10608eea1774b42b7695425ac6a699f3b4
lovethefeel <lovethefeel@gmail.com>
2022-05-11 00:54:19 +0900

    test(feat): @ConstructorBinding, @ConfigurationProperties 테스트

---
+++ profile-properties/src/test/java/com/lovethefeel/profile/config/ActiveProfileTest.java
@@ -0,0 +1,38 @@
+package com.lovethefeel.profile.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class ActiveProfileTest {
+
+    @Autowired
+    private ActiveProfile activeProfile;
+
+    @DisplayName("공통으로 설정된 port 번호를 확인한다.")
+    @Test
+    void serverPortTest() {
+        // then
+        assertThat(activeProfile.getPort()).isEqualTo(8080);
+    }
+
+    @DisplayName("현재 설정된 active profile의 application 설정값을 확인한다.")
+    @Test
+    void currentProfileTest() {
+        // then
+        assertThat(activeProfile.getProfile()).isEqualTo("dev");
+    }
+
+    @DisplayName("분리된 설정 파일을 임포트하여서 값을 확인한다.")
+    @Test
+    void externalApiUrl() {
+        // then
+        assertThat(activeProfile.getExternalApiUrl()).isEqualTo("http://dev-host:8080/api");
+    }
+}
