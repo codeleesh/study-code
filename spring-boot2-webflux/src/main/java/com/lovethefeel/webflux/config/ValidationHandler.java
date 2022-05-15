a8824047ad1009cd67bb6764c3f2dd964fc73443
lovethefeel <lovethefeel@gmail.com>
2022-05-15 22:55:58 +0900

    feat: (공통) 검증 에러 처리 로직 추가

---
+++ spring-boot2-webflux/src/main/java/com/lovethefeel/webflux/config/ValidationHandler.java
@@ -0,0 +1,24 @@
+package com.lovethefeel.webflux.config;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ValidationHandler {
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<List<String>> handleException(WebExchangeBindException e) {
+        final List<String> errors = e.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
