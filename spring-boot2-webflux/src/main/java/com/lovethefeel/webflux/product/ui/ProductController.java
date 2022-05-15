bf1666a291898a4d74c6eb3c9b30a9dd59995f7b
lovethefeel <lovethefeel@gmail.com>
2022-05-15 22:54:31 +0900

    feat: product controller 추가

---
+++ spring-boot2-webflux/src/main/java/com/lovethefeel/webflux/product/ui/ProductController.java
@@ -0,0 +1,29 @@
+package com.lovethefeel.webflux.product.ui;
+
+import com.lovethefeel.webflux.product.application.ProductService;
+import com.lovethefeel.webflux.product.dto.ProductRequest;
+import com.lovethefeel.webflux.product.dto.ProductResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+@RequestMapping("/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody ProductRequest productRequest) {
+        final ProductResponse productResponse = productService.saveProduct(productRequest);
+        return ResponseEntity.created(URI.create("/product/" + productResponse.getProductId())).body(productResponse);
+    }
+}
