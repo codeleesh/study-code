e277b30f08ce5c1b5bdd4113e35d50013e7b8223
lovethefeel <lovethefeel@gmail.com>
2022-05-15 22:56:16 +0900

    test(feat): 상품 텍스트 픽스쳐 추가

---
+++ spring-boot2-webflux/src/test/java/com/lovethefeel/webflux/fixture/TestProductFactory.java
@@ -0,0 +1,52 @@
+package com.lovethefeel.webflux.fixture;
+
+import com.lovethefeel.webflux.product.domain.Product;
+import com.lovethefeel.webflux.product.domain.ProductStatus;
+import com.lovethefeel.webflux.product.dto.ProductRequest;
+import com.lovethefeel.webflux.product.dto.ProductResponse;
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class TestProductFactory {
+    public static Product 상품(final Long productId, final String productName, final ProductStatus productStatus, final int productCount) {
+        return Product.from(productId, productName, productStatus, productCount);
+    }
+
+    public static ProductRequest 상품_등록_요청(final String productName, final ProductStatus productStatus, final int productCount) {
+        return ProductRequest.from(productName, productStatus, productCount);
+    }
+
+    public static void 상품_등록_생성됨(ProductResponse actual, Product expected) {
+        assertAll(
+                () -> assertThat(actual).isNotNull(),
+                () -> assertThat(actual.getProductId()).isEqualTo(expected.getId()),
+                () -> assertThat(actual.getProductName()).isEqualTo(expected.getName()),
+                () -> assertThat(actual.getProductStatus()).isEqualTo(expected.getStatus()),
+                () -> assertThat(actual.getProductCount()).isEqualTo(expected.getCount())
+        );
+    }
+
+    public static ProductResponse 상품_등록_응답(Long productId, String productName, ProductStatus productStatus, int productCount) {
+        return ProductResponse.from(productId, productName, productStatus, productCount);
+    }
+
+    public static ExtractableResponse<Response> 상품_등록_요청함(ProductRequest 상품_요청) {
+        return RestAssured
+                .given().log().all()
+                .body(상품_요청)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/product")
+                .then().log().all()
+                .extract();
+    }
+
+    public static void 상품_등록_생성됨(ExtractableResponse<Response> 상품_응답) {
+        assertThat(상품_응답.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+}
