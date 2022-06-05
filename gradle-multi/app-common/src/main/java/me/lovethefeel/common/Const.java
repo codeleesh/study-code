83781e82e7253434de42f399d5de128d9a78d243
lovethefeel <lovethefeel@gmail.com>
2022-06-05 14:44:46 +0900

    init multi-project

---
+++ gradle-multi/app-common/src/main/java/me/lovethefeel/common/Const.java
@@ -0,0 +1,12 @@
+package me.lovethefeel.common;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+@AllArgsConstructor
+public class Const {
+    public static final String STRING_EMPTY = "";
+}
