9331d6925dd7f910411979fe27b42e1093ae22c8
lovethefeel <lovethefeel@gmail.com>
2022-06-12 01:58:10 +0900

    refactor: 불필요한 설정으로 제거

--- profile-properties/src/main/java/com/lovethefeel/profile/config/PropertiesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.lovethefeel.profile.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(value = {MessageConfig.class})
-public class PropertiesConfiguration {
-}
