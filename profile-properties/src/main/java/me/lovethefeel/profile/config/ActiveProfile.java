f5dc90c7009a8a724e9aae98bfd8c165e0b392fb
lovethefeel <lovethefeel@gmail.com>
2022-06-13 02:05:04 +0900

    refactor: getter 추가

--- profile-properties/src/main/java/me/lovethefeel/profile/config/ActiveProfile.java
+++ profile-properties/src/main/java/me/lovethefeel/profile/config/ActiveProfile.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 public class ActiveProfile {
 
