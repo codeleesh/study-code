d0eaf16c47940885b1f06ede9da0f8d778d82f6b
lovethefeel <lovethefeel@gmail.com>
2022-07-23 13:01:00 +0900

    refactor: 비동기 콜백 처리

--- spring-boot-async/src/main/java/me/lovethefeel/member/application/MemberService.java
+++ spring-boot-async/src/main/java/me/lovethefeel/member/application/MemberService.java
@@ -8,7 +8,6 @@ import me.lovethefeel.member.dto.MemberRequest;
 import me.lovethefeel.member.dto.MemberResponse;
 import me.lovethefeel.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.util.concurrent.ListenableFuture;
 
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
@@ -25,24 +24,33 @@ public class MemberService {
 
     public MemberResponse certMember(final MemberRequest memberRequest) {
 
-        log.info("certProcess 호출전 - 1");
+        log.info("대기응답이 긴 Thread 호출전 - 1");
         final CompletableFuture<String> certResult = certService.certProcess(memberRequest.getMemberId());
         certResult.thenAccept(
-                r -> {
-                    log.info("--> non block result {}", r);
-                    log.info("certProcess 호출후 - 1");
+                result -> {
+                    log.info("대기응답이 긴 Thread 응답 결과 {}", result);
+                    if ("false".equals(result)) {
+                        log.warn("Cert 인증 실패");
+                        return ;
+                    }
+                    log.info("사용자 최종 상태 변경 호출전 - 4");
+                    memberRepository.registerById(memberRequest.getMemberId());
+                    log.info("사용자 최종 상태 변경 호출전 - 4");
+                    log.info("대기응답이 긴 Thread 호출 후 - 1");
                 }
         );
 
-        log.info("findById 호출전 - 2");
+        log.info("대기응답이 짧은 사용자 조회 호출전 - 2");
         final Member findMember = memberRepository.findById(memberRequest.getMemberId());
-        log.info("findById 호출후 - 2");
+        log.info("대기응답이 짧은 사용자 조회 호출후 - 2");
 
         if (Objects.nonNull(findMember)) {
             throw new IllegalArgumentException();
         }
 
+        log.info("대기응답이 짧은 사용자 저장 호출전 - 3");
         final Member saveMember = memberRepository.save(new Member(memberRequest.getMemberName()));
+        log.info("대기응답이 짧은 사용자 저장 호출후 - 3");
 
         return new MemberResponse().toEntity(saveMember);
     }
