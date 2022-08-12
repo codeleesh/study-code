d5378261a9588d2e2842615438bcbdbf784ef781
lovethefeel <lovethefeel@gmail.com>
2022-08-12 23:47:43 +0900

    refactor: Bean 찾는 파라미터 추가

--- jpa-history/src/main/java/me/lovethefeel/jpahistory/product/utils/BeanUtils.java
+++ jpa-history/src/main/java/me/lovethefeel/jpahistory/product/utils/BeanUtils.java
@@ -4,6 +4,12 @@ import org.springframework.context.ApplicationContext;
 
 public class BeanUtils {
 
+    public static Object getBean(final Class clazz) {
+
+        final ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
+        return applicationContext.getBean(clazz);
+    }
+
     public static Object getBean(final String beanName) {
 
         final ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
