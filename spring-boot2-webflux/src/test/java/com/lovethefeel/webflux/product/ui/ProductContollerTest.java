b231ed7b3010a92286f8e8203025657c7cb39561
lovethefeel <lovethefeel@gmail.com>
2022-08-19 23:11:38 +0900

    refactor(test): 통합테스트 응답결과 map으로 변환

--- spring-boot2-webflux/src/test/java/com/lovethefeel/webflux/product/ui/ProductContollerTest.java
+++ spring-boot2-webflux/src/test/java/com/lovethefeel/webflux/product/ui/ProductContollerTest.java
@@ -1,5 +1,7 @@
 package com.lovethefeel.webflux.product.ui;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lovethefeel.webflux.fixture.TestProductFactory;
 import com.lovethefeel.webflux.product.application.ProductService;
 import com.lovethefeel.webflux.product.domain.ProductStatus;
@@ -11,10 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -30,14 +36,14 @@ class ProductContollerTest {
 
     @DisplayName("상품 등록한다.")
     @Test
-    void createProductTest() {
+    void createProductTest() throws IOException {
         final ProductRequest 상품_요청 = TestProductFactory.상품_등록_요청("치킨", new BigDecimal("20000"), ProductStatus.ENABLE, 10);
         final ProductResponse 상품_응답 = TestProductFactory.상품_등록_응답(1L, "치킨", new BigDecimal("20000"), ProductStatus.ENABLE, 10);
 
         given(this.productService.saveProduct(any()))
                 .willReturn(상품_응답);
 
-        this.webTestClient.post().uri("/product")
+        EntityExchangeResult<byte[]> entityExchangeResult = this.webTestClient.post().uri("/product")
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(BodyInserters.fromValue(상품_요청))
                 .exchange()
@@ -46,10 +52,16 @@ class ProductContollerTest {
                         responseSpec -> responseSpec.expectHeader().contentType(MediaType.APPLICATION_JSON)
                 )
                 .expectBody()
-                .jsonPath("$.product_id").isEqualTo(1L)
-                .jsonPath("$.product_name").isEqualTo("치킨")
-                .jsonPath("$.product_status").isEqualTo("ENABLE")
-                .jsonPath("$.product_count").isEqualTo(10);
+                .returnResult();
+
+        final byte[] responseBody = entityExchangeResult.getResponseBody();
+        TypeReference<HashMap<String, String>> typeRef = new TypeReference<>() {};
+        HashMap<String, String> stringStringHashMap = new ObjectMapper().readValue(responseBody, typeRef);
+
+//                .jsonPath("$.product_id").isEqualTo(1L)
+//                .jsonPath("$.product_name").isEqualTo("치킨")
+//                .jsonPath("$.product_status").isEqualTo("ENABLE")
+//                .jsonPath("$.product_count").isEqualTo(10);
     }
 
     @DisplayName("상품 이름을 입력하지 않으면 예외를 발생한다.")
