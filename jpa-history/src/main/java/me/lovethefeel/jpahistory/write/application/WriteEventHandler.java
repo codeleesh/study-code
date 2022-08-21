57957671db44a594a2d92cfab7098523143d310b
lovethefeel <lovethefeel@gmail.com>
2022-08-21 21:17:38 +0900

    refactor: 메소드 리팩토링

--- jpa-history/src/main/java/me/lovethefeel/jpahistory/write/application/WriteEventHandler.java
+++ jpa-history/src/main/java/me/lovethefeel/jpahistory/write/application/WriteEventHandler.java
@@ -1,19 +1,35 @@
 package me.lovethefeel.jpahistory.write.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.lovethefeel.jpahistory.write.domain.Write;
+import me.lovethefeel.jpahistory.write.domain.WriteHistory;
+import me.lovethefeel.jpahistory.write.repository.WriteHistoryRepository;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import static me.lovethefeel.jpahistory.write.application.WriteEvent.*;
+import java.util.Objects;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class WriteEventHandler {
 
-    private final WriteService writeService;
+    private final WriteHistoryRepository historyRepository;
 
     @EventListener
-    public void writeHistory(final WriteHistory event) {
-        writeService.writeHistory(event.getWrite(), event.getComment());
+    public void writeHistory(final WriteEvent.WriteHistory event) {
+        this.saveWriteHistory(event.getWrite(), event.getComment());
+    }
+
+    private void saveWriteHistory(final Write write, final String comment) {
+
+        if (Objects.isNull(write)) {
+            return ;
+        }
+
+        log.info("saveWriteHistory write {} comment {}", write, comment);
+        final WriteHistory writeHistory = WriteHistory.fromEntity(write, comment);
+        historyRepository.save(writeHistory);
     }
 }
