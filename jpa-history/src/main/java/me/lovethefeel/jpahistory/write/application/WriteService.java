cf69d36cd645e6925ea63378989ece4e281a2202
lovethefeel <lovethefeel@gmail.com>
2022-08-21 21:17:31 +0900

    refactor: 메소드 리팩토링

--- jpa-history/src/main/java/me/lovethefeel/jpahistory/write/application/WriteService.java
+++ jpa-history/src/main/java/me/lovethefeel/jpahistory/write/application/WriteService.java
@@ -3,16 +3,12 @@ package me.lovethefeel.jpahistory.write.application;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.lovethefeel.jpahistory.write.domain.Write;
-import me.lovethefeel.jpahistory.write.domain.WriteHistory;
 import me.lovethefeel.jpahistory.write.dto.WriteRequest;
 import me.lovethefeel.jpahistory.write.dto.WriteResponse;
-import me.lovethefeel.jpahistory.write.repository.WriteHistoryRepository;
 import me.lovethefeel.jpahistory.write.repository.WriteRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Objects;
 
 
 @Slf4j
@@ -22,7 +18,6 @@ import java.util.Objects;
 public class WriteService {
 
     private final WriteRepository writeRepository;
-    private final WriteHistoryRepository historyRepository;
     private final ApplicationEventPublisher applicationEventPublisher;
 
     @Transactional
@@ -44,17 +39,5 @@ public class WriteService {
         applicationEventPublisher.publishEvent(WriteEvent.WriteHistory.of(findWrite, "이름 변경"));
 
         return WriteResponse.fromResponse(findWrite);
-    }
-
-    @Transactional
-    public void writeHistory(final Write write, final String comment) {
-
-        if (Objects.isNull(write)) {
-            log.warn("Write is null");
-            return ;
-        }
-
-        final WriteHistory writeHistory = WriteHistory.fromEntity(write, comment);
-        historyRepository.save(writeHistory);
     }
 }
