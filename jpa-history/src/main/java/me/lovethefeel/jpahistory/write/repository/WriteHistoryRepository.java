110acffc8ba7bbcf72ffcba267771fd8232d5d33
lovethefeel <lovethefeel@gmail.com>
2022-08-21 21:18:04 +0900

    refactor: 작가아이디로 조회 쿼리 추가

--- jpa-history/src/main/java/me/lovethefeel/jpahistory/write/repository/WriteHistoryRepository.java
+++ jpa-history/src/main/java/me/lovethefeel/jpahistory/write/repository/WriteHistoryRepository.java
@@ -3,5 +3,9 @@ package me.lovethefeel.jpahistory.write.repository;
 import me.lovethefeel.jpahistory.write.domain.WriteHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WriteHistoryRepository extends JpaRepository<WriteHistory, Long> {
+
+    List<WriteHistory> findByWriteId(final Long writeId);
 }
