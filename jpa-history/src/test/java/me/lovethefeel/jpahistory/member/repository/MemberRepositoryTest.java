ce24c365fe96ca556116eaeb5b981ee0897d5224
lovethefeel <lovethefeel@gmail.com>
2022-08-29 23:38:21 +0900

    refactor(test): 멤버 querydsl 테스트 추가

--- jpa-history/src/test/java/me/lovethefeel/jpahistory/member/repository/MemberRepositoryTest.java
+++ jpa-history/src/test/java/me/lovethefeel/jpahistory/member/repository/MemberRepositoryTest.java
@@ -1,16 +1,20 @@
 package me.lovethefeel.jpahistory.member.repository;
 
+import me.lovethefeel.jpahistory.config.TestConfig;
 import me.lovethefeel.jpahistory.member.domain.Member;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.*;
 
-@SpringBootTest
+@DataJpaTest
+@Import(TestConfig.class)
 @AutoConfigureTestDatabase(replace = NONE)
 class MemberRepositoryTest {
 
@@ -21,13 +25,24 @@ class MemberRepositoryTest {
     private MemberHistoryRepository historyRepository;
 
     @Test
-    @Transactional
-    void create_member_and_history() {
+    void 성공_멤버_저장() {
 
         final Member member = Member.fromCreate("lsh");
 
         final Member saveMember = memberRepository.save(member);
 
         assertThat(saveMember).isEqualTo(member);
+    }
+
+    @Test
+    void 성공_조회_querydsl_custom_기능확인() {
+
+        final String memberName = "lsh";
+        final Member member = Member.fromCreate(memberName);
+        memberRepository.save(member);
+
+        final List<Member> members = memberRepository.findByName(memberName);
+
+        assertThat(members).hasSize(1);
     }
 }
