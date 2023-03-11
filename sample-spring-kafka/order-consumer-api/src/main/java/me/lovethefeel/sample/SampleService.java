5980d3a6eb5c93e6793f0a9a25820972dae7cd57
michol.lee <lovethefeel@gmail.com>
2023-03-11 23:57:35 +0900

    refactor: 컨슈머 서비스 토픽 및 ack 처리

--- sample-spring-kafka/order-consumer-api/src/main/java/me/lovethefeel/sample/SampleService.java
+++ sample-spring-kafka/order-consumer-api/src/main/java/me/lovethefeel/sample/SampleService.java
@@ -3,10 +3,9 @@ package me.lovethefeel.sample;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.IOException;
 
 @Slf4j
 @Service
@@ -14,10 +13,11 @@ import java.io.IOException;
 @Transactional(readOnly = true)
 public class SampleService {
 
-    @KafkaListener(topics = "kafka-my-topic")
-    public void consume(String message) throws IOException {
+    @KafkaListener(topics = "test-topic1", groupId = "test-topic1-01", containerFactory = "defaultKafkaListenerContainerFactory")
+    public void consume(final String message, final Acknowledgment ack) {
         log.info("[consumer] Consumed message : {}", message);
         try {
+            ack.acknowledge(); // 모든 CRUD 작업이 완료되어야만 kafka의 read off-set 값 변경
             Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
