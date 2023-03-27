f10f77ad28f1464abbc6d8959ab6467351a3d2c8
michol.lee <lovethefeel@gmail.com>
2023-03-28 00:51:11 +0900

    feat: github feign client 설정

--- sample-spring-kafka/clients/src/main/java/me/lovethefeel/github/GithubFeignError.java
+++ sample-spring-kafka/clients/src/main/java/me/lovethefeel/github/GithubFeignError.java
@@ -9,7 +9,7 @@ public class GithubFeignError implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        log.info("");
+        log.info("test call");
         return null;
     }
 }
