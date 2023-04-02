f125b73904c3a6b92f888452f8cbe0b9a7b76924
michol.lee <lovethefeel@gmail.com>
2023-04-02 19:59:08 +0900

    refactor: 설명 추가

--- sample-spring-kafka/sample-api/src/main/java/me/lovethefeel/SampleApplication.java
+++ sample-spring-kafka/sample-api/src/main/java/me/lovethefeel/SampleApplication.java
@@ -2,7 +2,14 @@ package me.lovethefeel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * @EnableFeignClients 설명
+ * root package 에 있어야 하며, 그렇지 않은 경우 basePackages 또는 basePackageClasses 를 지정 필요
+ * 지정된 package 를 돌아다니면서 @FeignClient 를 찾아 구현체를 만들어 줍니다.
+ */
+@EnableFeignClients
 @SpringBootApplication
 public class SampleApplication {
 
